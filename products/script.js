0342a4afe1d4da6de9187891ba412fc8975cda06
Seeun Jung <seeun.jung01@gmail.com>
2025-03-25 09:50:39 +0900

    Feat: 무한 스크롤, 카트에 담기, 주문 기능 추가

--- products/script.js
+++ products/script.js
@@ -11,6 +11,10 @@ let pageNo = 1;
 const ITEM_NUMBER_PER_PAGE = 5;
 
 function showItems(){
+  const getCartItems = JSON.parse(localStorage.getItem("cartItems"));
+  let cartIds = [];
+  if(getCartItems) cartIds = getCartItems.map((item) => item.id);
+
   const container = document.getElementById("item-container");
   const showFrom = (pageNo -1) * ITEM_NUMBER_PER_PAGE;
   const showTil = pageNo * ITEM_NUMBER_PER_PAGE;
@@ -18,15 +22,20 @@ function showItems(){
 
   itemsShowing.forEach((item) => {
     const itemElement = document.createElement("li");
-    console.log(item);
+    itemElement.classList.add("show", "hidden");
     itemElement.innerHTML = ` <img src="../imgs/${item.productImgFileName}" />
       <div>
         <h2>${item.productName}</h2>
         <p>${item.productPrice}</p>
-        <button id="cart-${item.id}">Cart</button>
-        <button id="order-${item.id}">Order</button>
+        <button id="cart-${item.id}" class="cartAddBtn"
+          ${cartIds.includes(item.id) ? "disabled": ""}>Cart</button>
+        <button id="order-${item.id}" class="orderBtn">Order</button>
       </div>`;
     container.appendChild(itemElement);
+
+    setTimeout(() => {
+      itemElement.classList.remove("hidden");
+    }, 100);
   });
   pageNo++;
   setObserver();
@@ -50,14 +59,43 @@ function setObserver(){
     });
   }, options);
 
-  const lastItem = document.querySelector('.item:last-child');
-  if(lastPost){
-    observer.observe(lastPost);
+  const lastItem = document.querySelector('.show:last-child');
+  if(lastItem){
+    observer.observe(lastItem);
   }
 }
 
-
 fetchItems();
+
+
+ul.addEventListener("click", (event) => {
+  const itemId = event.target.id.split("-").pop();
+  if(event.target.className === "cartAddBtn"){
+    const getCartItems = JSON.parse(localStorage.getItem("cartItems"));
+    let newCart = [];
+    if(getCartItems){
+      newCart = [...getCartItems, items[itemId]];
+      alert(`${items[itemId].productName}가 장바구니에 추가됐습니다.`);
+    }
+    else{
+      newCart.push(items[itemId]);
+    }
+
+    localStorage.setItem("cartItems", JSON.stringify(newCart));
+    event.target.disabled = true;
+    return;
+  }
+
+  if(event.target.className === "orderBtn"){
+    sessionStorage.setItem("orderItem", JSON.stringify(items[itemId]));
+    const isConfirmed = confirm(
+      `${items[itemId].productName} 을 주문하시겠습니까?`
+    );
+    if(isConfirmed)
+      location.href = "../order/index.html";
+      return;
+  }
+});
 
 
 
